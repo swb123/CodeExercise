ead197c526d811f1c10af4c885f5f04b53515003
shiwenbo <1298032269@qq.com>
2019-09-27 01:55:58 +0800

    遍历二叉树的神级方法

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 + BinaryTreePrint-----打印二叉树的边界节点
 + BinaryTreePrint1----较为直观地打印二叉树
 + BinaryTreeSerial----二叉树地序列化和反序列化
++ BinaryTreeMorris----遍历二叉树的神级方法
 
 
---
+++ src/main/java/com/swb/binaryTree/BinaryTreeMorris.java
@@ -0,0 +1,142 @@
+package com.swb.binaryTree;
+
+/**
+ * 二叉树的Mirros遍历
+ * @author fangsanpi
+ * @date: 2019/9/27
+ */
+public class BinaryTreeMorris {
+    /**
+     * Mirros遍历
+     * @param head
+     */
+    public void morris(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            // 如果当前cur有左子树的话
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+                cur = cur.right;
+            }
+        }
+    }
+
+    /**
+     * 先序遍历（Mirros序列）
+     * @param head
+     */
+    public void morrisPre(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right  != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    System.out.print(cur.value + " ");
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            } else {
+                System.out.print(cur.value + " ");
+            }
+            cur = cur.right;
+
+        }
+    }
+    public void morrisIn(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right != null) {
+                    System.out.print(cur.value + " ");
+                    mostRight.right = null;
+                } else {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+            } else {
+                System.out.print(cur.value + " ");
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+    public void morrisPos(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                if (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printEdge(head);
+        System.out.println();
+    }
+    public static void printEdge(Node head) {
+        Node tail = reverseEdge(head);
+        Node cur = tail;
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.right;
+        }
+        reverseEdge(tail);
+    }
+    public static Node reverseEdge(Node from) {
+        Node pre = null;
+        Node next = null;
+        while (from != null) {
+            next = from.right;
+            from.right = pre;
+            pre =from;
+            from = next;
+        }
+        return pre;
+    }
+}
