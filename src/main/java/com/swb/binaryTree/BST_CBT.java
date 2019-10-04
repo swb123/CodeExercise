447e9c44997bc3e9055c14bc90a84cf6c2ba7525
shiwenbo <1298032269@qq.com>
2019-10-04 20:13:51 +0800

    判断一棵二叉树是否为搜索二叉树和完全二叉树

---
+++ src/main/java/com/swb/binaryTree/BST_CBT.java
@@ -0,0 +1,79 @@
+package com.swb.binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author fangsanpi
+ * @date: 2019/10/4
+ */
+public class BST_CBT {
+    /**
+     * 判断一棵树是否为二叉树
+     * @param head
+     * @return
+     */
+    public boolean isBST(Node head) {
+        if (head == null) {
+            return true;
+        }
+        boolean res = true;
+        Node pre = null;
+        Node cur1 = head;
+        Node cur2 = null;
+        while (cur1 != null) {
+            cur2 = cur1.left;
+            if (cur2 != null) {
+                while (cur2.right != null && cur2.right != cur1) {
+                    cur2 = cur2.right;
+                }
+                if ( cur2.right == null) {
+                    cur2.right = cur1;
+                    cur1 = cur1.left;
+                    continue;
+                } else {
+                    cur2.right = null;
+                }
+            }
+            if (pre != null && pre.value > cur1.value) {
+                res = false;
+            }
+            pre =cur1;
+            cur1 = cur1.right;
+        }
+        return res;
+    }
+
+    /**
+     * 判断一棵二叉树是否为完全二叉树
+     * @param head
+     * @return
+     */
+    public boolean isCBT(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        boolean leaf = false;
+        Node l = null;
+        Node r = null;
+        queue.offer(head);
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            l = head.left;
+            r = head.right;
+            if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
+                return false;
+            }
+            if (l != null) {
+                queue.offer(l);
+            }
+            if (r != null) {
+                queue.offer(r);
+            } else {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+}
